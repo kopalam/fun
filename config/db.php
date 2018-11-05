d2d85d6af295f20099f013003b7614d099b8aeb9
kopa <1113288996@qq.com>
2018-11-05 18:16:23 +0800

    Yii2基础

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=fun',
+    'username' => 'root',
+    'password' => '214883b225638557',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
